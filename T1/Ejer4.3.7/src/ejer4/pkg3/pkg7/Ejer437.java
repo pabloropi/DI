7f260477a10fa22ba472e5b33563c77d3c212b71
pabloropi <pabloski07@gmail.com>
2020-10-05 14:27:04 +0200

    Start

--- T1/Ejer4.3.7/src/ejer4/pkg3/pkg7/Ejer437.java
+++ T1/Ejer4.3.7/src/ejer4/pkg3/pkg7/Ejer437.java
@@ -5,11 +5,14 @@
  */
 package ejer4.pkg3.pkg7;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.shape.Ellipse;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 /**
  *
@@ -19,15 +22,32 @@ public class Ejer437 extends Application {
     
     @Override
     public void start(Stage primaryStage) {
+        Pane root = new Pane();
         
         Ellipse ellipse = new Ellipse(10, 10, 100, 50);
         
-        StackPane root = new StackPane();
+        ellipse.centerXProperty().bind(root.widthProperty().divide(2));
+        ellipse.centerYProperty().bind(root.heightProperty().divide(2));
+        
         root.getChildren().add(ellipse);
+        
+        FadeTransition ft = new FadeTransition();
+        ft.setNode(ellipse);
+        ft.setDuration(Duration.millis(3000));
+        ft.setFromValue(1.0);
+        ft.setToValue(0.1);
+        ft.setCycleCount(Timeline.INDEFINITE);
+        ft.setAutoReverse(true);
+        ft.play();
+        
+        ellipse.setOnMousePressed(e->ft.pause());
+        ellipse.setOnMouseReleased(e->ft.play());
+        
+        
         
         Scene scene = new Scene(root, 300, 250);
         
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("FadeTransition");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
