ed395272886115172d0f40002c7ac84df522964c
pabloropi <pabloski07@gmail.com>
2020-10-06 17:59:50 +0200

    V1

--- T1/Ejer4.3.8/src/ejer4/pkg3/pkg8/Ejer438.java
+++ T1/Ejer4.3.8/src/ejer4/pkg3/pkg8/Ejer438.java
@@ -5,35 +5,83 @@
  */
 package ejer4.pkg3.pkg8;
 
+import com.sun.javafx.perf.PerformanceTracker;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 /**
  *
  * @author Usuario
  */
+
 public class Ejer438 extends Application {
+    
+    public static double ballspeedx = 1;
+    public static double ballspeedy = 1;
     
     @Override
     public void start(Stage primaryStage) {
         
+        // Etiqueta con los FPS
         Label label = new Label();
         
-        Circle circle = new Circle(150, 125, 10);
-        circle.setStroke(Color.BLACK);
+        // Bola
+        Circle ball = new Circle(10);
+        ball.setTranslateX(450 *0.5);
+        ball.setTranslateY(350 *0.5);
+        ball.setStroke(Color.BLACK);
 
+        // Pane y escena
         Pane root = new Pane();
-        root.getChildren().addAll(label, circle);
+        root.getChildren().addAll(label, ball);
+       
+        Scene scene = new Scene(root, 450, 350);
+        
+        // Escuchador a incluir en el bucle de Timeline
+        EventHandler<ActionEvent> eH = e->{
+            
+            // Mostrar la frecuencia de refresco FPS
+            PerformanceTracker perfTracker = PerformanceTracker.getSceneTracker(scene);
+            
+            label.setText("FPS (Timeline) = "+perfTracker.getInstantFPS());
+            
+            // Cambiar la dirección de la bola si llega a los extremos
+            if(ball.getTranslateX() < 10 ||ball.getTranslateX() > 440)
+            {
+                ballspeedx *=-1;
+            }
+            
+            ball.setTranslateX(ball.getTranslateX()+ ballspeedx);
+            
+            if(ball.getTranslateY() < 10 ||ball.getTranslateY() > 340)
+            {
+                ballspeedy *=-1;
+            }
+            
+            ball.setTranslateY(ball.getTranslateY()+ ballspeedy);
+        };
         
         
-        Scene scene = new Scene(root, 300, 250);
+        /* Definimos el bucle con la duración, cada 5 milisegundos que son
+        aproximadamente 60 FPS */
+        Timeline animation = new Timeline(new KeyFrame(Duration.millis(5),eH));
+        animation.setCycleCount(Timeline.INDEFINITE);
         
-        primaryStage.setTitle("Hello World!");
+        // iniciamos animation
+        animation.play();
+
+        
+        primaryStage.setTitle("Bouncing Ball");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
